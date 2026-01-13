be9b869bd3dbd6222bf3cf1c9d51c54b2b51c823
Javier020301 <javiesaransig2@gmail.com>
2026-01-13 17:28:36 -0500

    implementacion de backend v1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Maquina(BaseModel):
+    codigo_equipo: str
+    estado_equipo: str
+    area_equipo: str
+    fecha_adquision: str
+
+
+maquinas: List[Maquina] = []
+
+@app.post('/maquinas',tags=['maquinas'])
+def agregar_maquina(maquina:Maquina):
+    maquinas.append(maquina)
+    return [m.model_dump() for m in maquinas]
